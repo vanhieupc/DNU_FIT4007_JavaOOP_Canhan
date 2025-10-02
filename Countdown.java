472083afad5c6bc618c0135679778a672c585364
vanhieupc <ngovanhieu2006pc@gmail.com>
2025-10-03 00:28:17 +0700

    Add files via upload

---
+++ Countdown.java
@@ -0,0 +1,9 @@
+public class Countdown {
+    public static void main(String[] args) {
+        int N = 10; // Gán trực tiếp số nguyên dương N
+
+        for (int i = N; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
