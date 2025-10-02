94cf7b435a9a28b34328b93c753284e7a9c16277
vanhieupc <ngovanhieu2006pc@gmail.com>
2025-10-03 00:28:41 +0700

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,9 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int N = 20; // Gán trực tiếp số nguyên dương N
+
+        for (int i = 2; i <= N; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
