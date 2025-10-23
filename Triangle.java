b783369c4f9af1f20cb198c68fda4d95b2d7d73d
vanhieupc <ngovanhieu2006pc@gmail.com>
2025-10-24 00:52:49 +0700

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,41 @@
+public class Triangle {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    // Getter và Setter cho sideA
+    public double getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    // Getter và Setter cho sideB
+    public double getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
+    }
+
+    public String toString() {
+        return "Triangle{sideA=" + sideA + ", sideB=" + sideB + ", sideC=" + sideC + "}";
+    }
+}
