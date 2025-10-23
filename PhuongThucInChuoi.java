f0e77e17bdf832ad45c59baabf872f2d4a96866d
vanhieupc <ngovanhieu2006pc@gmail.com>
2025-10-24 00:34:14 +0700

    Add files via upload

---
+++ PhuongThucInChuoi.java
@@ -0,0 +1,11 @@
+public class PhuongThucInChuoi {
+    public static void inChuoi(String s, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        inChuoi("Xin chào", 3);
+    }
+}
