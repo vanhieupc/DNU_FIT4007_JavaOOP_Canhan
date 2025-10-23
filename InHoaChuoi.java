4e5b0a66b807f1af62703edab6e97ab4265cd5a6
vanhieupc <ngovanhieu2006pc@gmail.com>
2025-10-23 17:22:35 +0700

    Add files via upload

---
+++ InHoaChuoi.java
@@ -0,0 +1,9 @@
+public class InHoaChuoi {
+    public static void main(String[] args) {
+        String chuoi = "java là ngôn ngữ lập trình";
+        String chuoiInHoa = chuoi.toUpperCase();
+
+        System.out.println("Chuỗi ban đầu: " + chuoi);
+        System.out.println("Chuỗi in hoa: " + chuoiInHoa);
+    }
+}
